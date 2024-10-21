f4bee6a45a681c453ce3a1bf0178e166b21ac54a
Jenaro Calviño <jen.calvineo@gmail.com>
2024-10-21 15:48:19 -0300

    react dropzone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,161 @@
-import GradeForm from "@/components/grade-form";
 import Link from "next/link";
+import type { FormState } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { ChangeEvent, FormEvent, useCallback, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+
+function usePasteEvent(pasteListener: (event: ClipboardEvent) => void) {
+  useEffect(() => {
+    document.addEventListener("paste", pasteListener);
+
+    return () => {
+      document.removeEventListener("paste", pasteListener);
+    };
+  }, [pasteListener]);
+}
+
+const loadingSentences = [
+  "Analizando tu CV...",
+  "Buscando áreas a mejorar...",
+  "Puntuando tus habilidades...",
+  "Generando sugerencias...",
+];
 
 export default function Home() {
+  const router = useRouter();
+
+  const mutation = useMutation<
+    FormState,
+    Error,
+    | { url: string; formData?: undefined }
+    | { formData: FormData; url?: undefined }
+  >({
+    mutationKey: ["resume-check"],
+    mutationFn: async ({ url, formData }) => {
+      if (url && (!url.startsWith("https") || !url.endsWith(".pdf"))) {
+        throw new Error("Url must start with https and end with pdf");
+      }
+
+      if (formData) {
+        return fetch("/api/grade", {
+          method: "POST",
+          body: formData,
+        }).then((b) => b.json());
+      } else {
+        return fetch("/api/grade?url=" + url, {
+          method: "POST",
+        }).then((b) => b.json());
+      }
+    },
+    onSuccess: () => {
+      router.push("/review");
+    },
+  });
+
+  const onDrop = useCallback(
+    (files: File[]) => {
+      const formData = new FormData();
+      const [cvFile] = files;
+
+      if (!cvFile) return;
+
+      formData.set("resume", cvFile);
+      mutation.mutate({ formData });
+    },
+    [mutation],
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { file: [".pdf"] },
+  });
+
+  usePasteEvent(async (event: ClipboardEvent) => {
+    event.preventDefault();
+    const data = event.clipboardData;
+    if (!data) {
+      return;
+    }
+
+    const url = data.getData("text").toString();
+    mutation.mutate({ url });
+  });
+
+  async function handleFormSubmission(event: ChangeEvent) {
+    const formElement = event.currentTarget.parentElement;
+    if (!formElement || !(formElement instanceof HTMLFormElement)) return;
+    const formData = new FormData(formElement);
+    const honeypot = formData.get("name");
+
+    if (honeypot) {
+      return;
+    }
+
+    mutation.mutate({ formData });
+  }
+
+  function prevent(event: FormEvent) {
+    event.preventDefault();
+  }
+
   return (
-    <>
-      <GradeForm />
-      <p>
+    <div
+      className={`container items-center justify-start flex flex-col h-full p-8 border-4 rounded-xl ${isDragActive ? "cursor-grabbing border-gray-300 border-dashed" : "border-transparent"}`}
+      {...getRootProps()}
+    >
+      <form
+        onSubmit={prevent}
+        method="POST"
+        action="/api/grade"
+        encType="multipart/form-data"
+        className={`w-full overflow-hidden max-w-[300px] p-8 relative border-2 rounded-lg border-gray-800 border-dashed flex items-center justify-center flex-col gap-1 ${mutation.isPending ? "animate-pulse" : ""}`}
+      >
+        <span
+          className={`px-4 py-2 block rounded-xl bg-indigo-800 font-bold hover:bg-indigo-600 cursor-pointer ${mutation.isPending ? "bg-gray-700 text-gray-400" : ""}`}
+        >
+          Buscar
+        </span>
+        <span className="text-gray-300 mt-4">o arrastrá un archivo,</span>
+        <span className="text-gray-300 text-sm">o pegá un url</span>
+        <input
+          className="sr-only"
+          onChange={handleFormSubmission}
+          disabled={mutation.isPending}
+          id="resume"
+          name="resume"
+          {...getInputProps()}
+        />
+        {/* honeypot */}
+        <input className="sr-only" type="text" name="name" />
+        {mutation.error ? (
+          <div>
+            <p className="text-red-500 mb-2">{mutation.error.message}</p>
+          </div>
+        ) : null}
+      </form>
+
+      {mutation.isPending ? (
+        <div className="mt-4 max-h-8 overflow-hidden">
+          <div
+            /** @ts-expect-error we are using css props the proper way */
+            style={{ "--loading-steps": loadingSentences.length }}
+            className="animate-loading"
+          >
+            {loadingSentences.map((s) => (
+              <p
+                /** @ts-expect-error we are using css props the proper way */
+                style={{ "--to": s.length }}
+                className="h-8 m-0 relative overflow-hidden thinking w-max text-gray-600 text-center"
+                key={s}
+              >
+                {s}
+              </p>
+            ))}
+          </div>
+        </div>
+      ) : null}
+      <p className="text-center mt-6">
         Revisá tu curriculum basado en las guías de{" "}
         <Link
           href="https://docs.silver.dev/interview-ready/getting-started/preparando-el-CV"
@@ -14,6 +164,6 @@ export default function Home() {
           Silver.dev
         </Link>
       </p>
-    </>
+    </div>
   );
 }
