4a6fafc82ba17f40e9ff031bd4eac49950b1f05b
Jenaro Calviño <jen.calvineo@gmail.com>
2024-11-12 12:40:24 -0300

    fix: dismiss size

--- src/components/error-badge.tsx
+++ src/components/error-badge.tsx
@@ -12,9 +12,9 @@ export default function ErrorBadge({ error }: { error: Error | null }) {
   }, [error]);
 
   return error && !d ? (
-    <div className="p-6 bg-red-600 rounded-lg w-max absolute top-28 left-1/2 translate-x-[-50%] animate-error z-10">
+    <div className="shadow-md px-6 py-7 bg-red-600 text-white rounded-lg w-max absolute top-28 left-1/2 translate-x-[-50%] animate-error z-10">
       <p className="text-center">{error.message}</p>
-      <button onClick={dismiss} className="absolute top-1 right-3">
+      <button onClick={dismiss} className="absolute top-1 right-3 text-xl">
         &times;
       </button>
     </div>
