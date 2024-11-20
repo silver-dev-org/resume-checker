aba979e8fa5c6bd69288616989b127188c3a8319
Jenaro Calviño <jen.calvineo@gmail.com>
2024-11-20 12:42:43 -0300

    fix: change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,14 @@ export default function Document() {
       <Head>
         <link
           rel="icon"
-          type="image/svg"
-          href="images/logo-white.svg"
+          type="image/png"
+          href="images/favicon-32x32.png"
           sizes="32x32"
         />
         <link
           rel="icon"
-          type="image/svg"
-          href="images/logo-white.svg"
+          type="image/png"
+          href="images/favicon-16x16.png"
           sizes="16x16"
         />
       </Head>
