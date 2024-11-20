4f28f0fe64bf791940d4c94b19f465d4fea1843e
Jenaro Calviño <jen.calvineo@gmail.com>
2024-11-20 12:02:36 -0300

    feat: added footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div
       className={
-        lato.className + " grid grid-rows-[auto_1fr_100px] min-h-screen"
+        lato.className + " grid grid-rows-[auto_1fr_auto] min-h-screen"
       }
     >
       <Head>
@@ -30,6 +30,20 @@ export default function Layout({ children }: { children: ReactNode }) {
       <main className="flex flex-col gap-8 row-start-2 items-center py-6 px-2 justify-center">
         {children}
       </main>
+      <footer className="border-t-2 border-t-gray-800 p-4 flex items-center justify-center gap-4">
+        <Link
+          href="https://jobs.ashbyhq.com/Silver?utm_source=Pedw1mQEZd"
+          className="text-sm text-indigo-400 cursor-pointer hover:text-indigo-300"
+        >
+          Open Jobs
+        </Link>
+        <Link
+          href="https://ready.silver.dev/"
+          className="text-sm text-indigo-400 cursor-pointer hover:text-indigo-300"
+        >
+          Interview Ready
+        </Link>
+      </footer>
     </div>
   );
 }
