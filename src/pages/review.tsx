3786439a20fcad4462ad76b5df6593c887973702
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-11-21 11:29:33 -0300

    margin change

--- src/pages/review.tsx
+++ src/pages/review.tsx
@@ -163,7 +163,7 @@ export default function Review() {
         </form>
       </div>
       <hr className="w-full my-8 lg:col-span-2" />
-      <h2 className="w-full my-8 text-3xl text-center lg:col-span-2">
+      <h2 className="w-full my-6 text-3xl text-center lg:col-span-2">
         Resume Checker es de{" "}
         <Link
           href="https://ready.silver.dev"
