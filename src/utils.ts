963b81d246bd181f83feca1d6bac1330670c93ae
Gaspar Dip <gaspar.dip@hotmail.com>
2024-12-28 02:54:30 -0300

    bump typst template version to latest

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
-const TYPST_TEMPLATE_VERSION = "1.0.1";
+const TYPST_TEMPLATE_VERSION = "1.0.2";
 export const TYPST_TEMPLATE_URL = `https://typst.app/app?template=silver-dev-cv&version=${TYPST_TEMPLATE_VERSION}`;
